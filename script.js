44b909a48d0452b277394ba7f4ea3ecc86f55330
theCieniu <pweiher@wp.eu>
2019-08-28 22:37:07 +0200

    responsive navigation menu
    media query, javascript, for mobile and for laptop screen

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-let randomForm = document.getElementById("random")
+let burger = document.getElementById("burger");
+let menu = document.getElementById("menu");
+let overlay = document.getElementById("overlay");
 
-randomForm.addEventListener("submit", (e) => {
-    let minimal = document.getElementById("range-min").value;
-  let mini = parseInt(minimal);
-  let maximum = document.getElementById("range-max").value;
-  let maxi = parseInt(maximum);
-  let randomResult = Math.floor(Math.random() * (maxi - mini + 1) + mini);
-  let writeRandom = `<p>Your random number from ${mini} to ${maxi} is <strong>${randomResult}</strong>.</p>`;
-  randomForm.insertAdjacentHTML("beforeend", writeRandom);
-  e.preventDefault();
-})
+burger.addEventListener("click", opened);
+
+overlay.addEventListener("click", closed);
+
+menu.addEventListener("click", closed);
+
+function opened() {
+  overlay.classList.remove("open");
+  menu.classList.remove("open");
+  burger.classList.add("open");
+  console.log("open")
+}
+
+function closed() {
+  overlay.classList.add("open");
+  menu.classList.add("open");
+  burger.classList.remove("open");
+  console.log("closed")
+}
